662e3b86c2088c68abba25b1763d02951beab6b4
FioreDT <fiodito32@gmail.com>
2023-06-06 22:35:44 -0300

    primercommit

---
+++ miprimerapagina/index.js
@@ -0,0 +1,12 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
+
+
+
+const miBoton = document.getElementById("miBoton")
+function handleClick(){alert("¡Hola! Has hecho click en el botón.");}
+miBoton.addEventListener("click", handleClick )
+
